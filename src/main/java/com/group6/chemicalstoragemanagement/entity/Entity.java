24f129aaf02e774f2d781d067f4799fc087df5ad
Phạm Tiến <phamtanminhtien@gmail.com>
2022-05-26 09:17:31 +0700

    da

--- src/main/java/com/group6/chemicalstoragemanagement/entity/Entity.java
+++ src/main/java/com/group6/chemicalstoragemanagement/entity/Entity.java
@@ -4,6 +4,7 @@ package com.group6.chemicalstoragemanagement.entity;
 
 
 import java.lang.reflect.Field;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -12,13 +13,18 @@ import java.util.UUID;
 public class Entity {
     static Long count = 0L;
     public Long ID;
-
+    public Date createAt;
     public Entity() {
         Entity.count++;
         this.ID = Entity.count;
+        this.createAt = new Date();
     }
 
     public Long getID() {
         return ID;
+    }
+
+    public Date getCreateAt(){
+        return createAt;
     }
 }
